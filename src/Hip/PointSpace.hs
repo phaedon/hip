54cedeb6c81526f0d5aefffa90d2b33327cd0b85
Phaedon Sinis <phaedonsinis@gmail.com>
2011-11-05 23:54:05 -0700

    cleaning up PointSpace from the merge

--- src/Hip/PointSpace.hs
+++ src/Hip/PointSpace.hs
@@ -1,29 +1,8 @@
 module Hip.PointSpace where
 
--- import qualified Data.Vector.Unboxed as VU
-
-{-
-type Point t = VU.Vector t
-type IPoint = Point Int
-type DPoint = Point Double
-
-pAdd :: (VU.Unbox t, Num t) => Point t -> Point t -> Point t
-pAdd a b = VU.zipWith (+) a b
-
-pMult :: (VU.Unbox t, Num t) => Point t -> Point t -> Point t
-pMult a b = VU.zipWith (*) a b
-
-sMult :: Double -> DPoint -> DPoint
-sMult s pt = VU.map (*s) pt
-
-lerp :: Double -> DPoint -> DPoint -> DPoint
-lerp alpha a b = pAdd (sMult alpha a) (sMult (1 - alpha) b)
--}
 
 -- | We use Double for coordinates so that non-integer transforms
 --   and coordinate accesses are feasible. (Will be helpful for
---   antialiasing later on.) 
-
 data Point2d = Point2d !Double !Double
      deriving (Show, Eq)
 
@@ -34,8 +13,8 @@ instance Point Point2d where
          lerp s p1 p2 = pAdd (sMult s p1) (sMult (1 - s) p2)
          pNeg (Point2d x y) = Point2d (-x) (-y)
 
--- | Generic Point typeclass.
 
+-- | Generic Point typeclass.
 class Point t where
 
       -- Addition (wtd averages, translations, etc)
@@ -43,7 +22,7 @@ class Point t where
 
       -- Pairwise multiplication (i.e. for rotations)
       pMult :: t -> t -> t
-
+      
       -- Scalar multiplication (i.e. for taking weighted averages)
       sMult :: Double -> t -> t
 
